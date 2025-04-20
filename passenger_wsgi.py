39323f4036589de98801c45e5b584fccdaaa5b37
KumaloWilson <kumalowilson900@gmail.com>
2025-04-20 14:11:33 +0200

    done and dusteddd

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -10,8 +10,5 @@ path = os.path.dirname(os.path.abspath(__file__))
 if path not in sys.path:
     sys.path.insert(0, path)
 
-# Import your Flask app
+# Import your Flask app from run.py
 from run import app as application
-
-# This is required for WSGI compatibility
-application = application
