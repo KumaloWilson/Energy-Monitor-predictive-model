a5cab8da8e6d54aeee7e01c4de63f1fd46b24c12
KumaloWilson <kumalowilson900@gmail.com>
2025-04-20 10:59:57 +0200

    added templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
@@ -24,6 +24,9 @@ def create_app(config_name='default'):
     # Register blueprints
     from app.views.api import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
+
+    def index():
+        return render_template('index.html')
     
     # Create database tables
     with app.app_context():
